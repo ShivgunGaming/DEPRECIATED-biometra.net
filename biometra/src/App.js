e588a452c3a7a0f7b337934f17957a475143b457
Shivan <102505925+ShivgunGaming@users.noreply.github.com>
2023-05-30 14:50:39 +1000

    Add files via upload

---
+++ biometra/src/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import { useState } from "react";
+import logo from "./biologothree.png";
+import { Select } from "antd";
+import { Switch } from "antd";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import CreateAccount from "./components/CreateAccount.js";
+import RecoverAccount from "./components/RecoverAccount.js";
+import WalletView from "./components/WalletView.js";
+
+function App() {
+  const [wallet, setWallet] = useState(null);
+  const [seedPhrase, setSeedPhrase] = useState(null);
+  const [selectedChain, setSelectedChain] = useState("0x1");
+  // const [theme, setTheme] = useState("light");
+
+  /* const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    // You can add additional logic here to save the theme preference in local storage or a user preference store.
+    <Switch checked={theme === "dark"} onChange={toggleTheme} /> Return statement
+  };
+  */
+
+  return (
+    <div className = "App">
+      <header>
+        <img src={logo} className="headerLogo" alt="logo" />
+        <div className="themeToggler">
+      </div>
+        <Select
+          onChange={(val) => setSelectedChain(val)}
+          value={selectedChain}
+          options={[
+            { label: "Ethereum", value: "0x1" },
+            { label: "Mumbai Testnet", value: "0x13881" },
+            { label: "Polygon", value: "0x89" },
+            { label: "Avalanche", value: "0xa86a" },
+            { label: "Binance", value: "0x38" },
+          ]}
+          className="dropdown"
+        ></Select>
+      </header>
+      {wallet && seedPhrase ? (
+        <Routes>
+          <Route
+            path="/yourwallet"
+            element={
+              <WalletView
+                wallet={wallet}
+                setWallet={setWallet}
+                seedPhrase={seedPhrase}
+                setSeedPhrase={setSeedPhrase}
+                selectedChain={selectedChain}
+              />
+            }
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/recover"
+            element={
+              <RecoverAccount
+                setSeedPhrase={setSeedPhrase}
+                setWallet={setWallet}
+              />
+            }
+          ></Route>
+          <Route
+            path="/yourwallet"
+            element={
+              <CreateAccount
+                setSeedPhrase={setSeedPhrase}
+                setWallet={setWallet}
+              />
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ biometra/src/chains.js
@@ -0,0 +1,43 @@
+const Ethereum = {
+    hex: '0x1',
+    name: 'Ethereum',
+    rpcUrl: 'Enter your rpc url here',
+    ticker: 'ETH'
+  };
+  
+  const MumbaiTestnet = {
+    hex: '0x13881',
+    name: 'Mumbai Testnet',
+    rpcUrl: 'Enter your rpc url here',
+    ticker: 'MATIC'
+  };
+  
+  const Polygon = {
+    hex: '0x89',
+    name: 'Polygon Mainnet',
+    rpcUrl: 'Enter your rpc url here',
+    ticker: 'MATIC'
+  };
+  
+  const Avalanche = {
+    hex: '0xa86a',
+    name: 'Avalanche Mainnet',
+    rpcUrl: 'Enter your rpc url here',
+    ticker: 'AVAX'
+  };
+  
+  const Binance = {
+    hex: '0x38',
+    name: 'Binance Mainnet',
+    rpcUrl: 'Enter your rpc url here',
+    ticker: 'BNB'
+  };
+  
+  export const CHAINS_CONFIG = {
+    '0x1': Ethereum,
+    '0x13881': MumbaiTestnet,
+    '0x89': Polygon,
+    '0xa86a': Avalanche,
+    '0x38': Binance,
+  };
+  
