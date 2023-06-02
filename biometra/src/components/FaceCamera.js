cdc50f31ba0168836cbeb955a5d0efcff62373ae
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-02 20:29:54 +1000

    Refactored FaceCamera.js

--- biometra/src/components/FaceCamera.js
+++ biometra/src/components/FaceCamera.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "@innovatrics/dot-face-auto-capture";
-import { useEffect } from "react";
 
 function FaceCamera(props) {
   useEffect(() => {
-    const faceAutoCaptureHTMLElement = document.getElementById(
-      "x-dot-face-auto-capture"
-    );
+    const initializeFaceAutoCapture = () => {
+      const faceAutoCaptureHTMLElement = document.getElementById(
+        "x-dot-face-auto-capture"
+      );
 
-    if (faceAutoCaptureHTMLElement) {
-      faceAutoCaptureHTMLElement.cameraOptions = props;
-    }
-  });
+      if (faceAutoCaptureHTMLElement) {
+        faceAutoCaptureHTMLElement.cameraOptions = props;
+      }
+    };
 
-  return React.createElement("x-dot-face-auto-capture", {
-    id: "x-dot-face-auto-capture",
-  });
+    initializeFaceAutoCapture();
+  }, [props]);
+
+  return <x-dot-face-auto-capture id="x-dot-face-auto-capture" />;
 }
 
 export default FaceCamera;
