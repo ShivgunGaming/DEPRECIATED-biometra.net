8f279c7fd1337e7c1158a2258b37f279cb114603
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-02 19:28:58 +1000

    Refactored FaceAutoCapture component

--- biometra/src/components/FaceAutoCapture.js
+++ biometra/src/components/FaceAutoCapture.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../styles/index.module.css";
 import buttonStyles from "../styles/button.module.css";
 import FaceCamera from "./FaceCamera";
