a2a6873871a10aa7eabfb7b8199c45c3eb7cfe15
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-03 11:50:34 +1000

    Innovatrics face scanning component integrated to App

--- biometra/src/App.js
+++ biometra/src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import "./App.css";
-import { useState, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import logo from "./biologothree.png";
 import { Select } from "antd";
 import { Routes, Route } from "react-router-dom";
@@ -7,69 +8,38 @@ import Home from "./components/Home";
 import CreateAccount from "./components/CreateAccount.js";
 import RecoverAccount from "./components/RecoverAccount.js";
 import WalletView from "./components/WalletView.js";
-import ComponentSelect from "./components/ComponentSelect";
-import DocumentAutoCapture from "./components/DocumentAutoCapture";
-import FaceAutoCapture from "./components/FaceAutoCapture";
-import PhotoResult from "./components/PhotoResult";
-import styles from "./styles/index.module.css";
+import "@innovatrics/dot-face-auto-capture";
+
+const FaceCamera = (props) => {
+  useEffect(() => {
+    const faceAutoCaptureHTMLElement = document.getElementById(
+      "x-dot-face-auto-capture"
+    );
+
+    if (faceAutoCaptureHTMLElement) {
+      faceAutoCaptureHTMLElement.cameraOptions = props;
+    }
+  });
+
+  return React.createElement("x-dot-face-auto-capture", { id: "x-dot-face-auto-capture" });
+};
 
 function App() {
   const [wallet, setWallet] = useState(null);
   const [seedPhrase, setSeedPhrase] = useState(null);
   const [selectedChain, setSelectedChain] = useState("0x1");
-  const [step, setStep] = useState("FACE_CAPTURE"); // Display "FACE_CAPTURE" step first
-  const [photoUrl, setPhotoUrl] = useState();
 
-  const handlePhotoTaken = (image, data, content) => {
-    const imageUrl = URL.createObjectURL(image);
-    setPhotoUrl(imageUrl);
+  const handleFacePhotoTaken = (image, resolution) => {
+    console.log("Face image:", image);
+    console.log("Resolution:", resolution);
   };
 
-  const handleDocumentPhotoTaken = (image, data) => {
-    handlePhotoTaken(image, data);
-  };
-
-  const handleFaceCapturePhotoTaken = (image, data) => {
-    handlePhotoTaken(image, data);
-  };
-
-  const handleError = useCallback((error) => {
-    alert(error);
-  }, []);
-
-  const handleBackClick = () => {
-    setPhotoUrl(undefined);
-    setStep("SELECT_COMPONENT");
-  };
-
-  const renderStep = (currentStep) => {
-    switch (currentStep) {
-      case "DOCUMENT_CAPTURE":
-        return (
-          <>
-            <DocumentAutoCapture
-              onPhotoTaken={handleDocumentPhotoTaken}
-              onError={handleError}
-              onBackClick={handleBackClick}
-            />
-            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
-          </>
-        );
-      case "FACE_CAPTURE":
-        return (
-          <>
-            <FaceAutoCapture
-              onPhotoTaken={handleFaceCapturePhotoTaken}
-              onError={handleError}
-              onBackClick={handleBackClick}
-            />
-            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
-          </>
-        );
-      default:
-        return <ComponentSelect setStep={setStep} />;
-    }
-  };
+  const handleError = useCallback(
+    (error) => {
+      alert(error);
+    },
+    []
+  );
 
   return (
     <div className="App">
@@ -106,7 +76,7 @@ function App() {
         </Routes>
       ) : (
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
           <Route
             path="/recover"
             element={
@@ -115,22 +85,26 @@ function App() {
                 setWallet={setWallet}
               />
             }
-          ></Route>
+          />
           <Route
             path="/yourwallet"
             element={
-              <CreateAccount
-                setSeedPhrase={setSeedPhrase}
-                setWallet={setWallet}
-              />
+              <>
+                <CreateAccount
+                  setSeedPhrase={setSeedPhrase}
+                  setWallet={setWallet}
+                />
+                <FaceCamera
+                  imageType="png"
+                  cameraFacing="user"
+                  onPhotoTaken={handleFacePhotoTaken}
+                  onError={handleError}
+                />
+              </>
             }
           />
         </Routes>
       )}
-      <div className={styles.app}>
-        <h1>DOT Web Components Integration</h1>
-        {renderStep(step)}
-      </div>
     </div>
   );
 }
