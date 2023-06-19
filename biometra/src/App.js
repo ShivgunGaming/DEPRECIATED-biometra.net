bc3253205e271440de9ef625f100a27104d82e48
Papagun <102505925+ShivgunGaming@users.noreply.github.com>
2023-06-19 14:54:12 +1000

    Update App.js

--- biometra/src/App.js
+++ biometra/src/App.js
@@ -1,15 +1,14 @@
-//App.js
-import React, { useEffect } from "react";
-import "./App.css";
-import { useState, useCallback } from "react";
-import logo from "./biologothree.png";
-import { Select } from "antd";
+import React, { useEffect, useState, useCallback } from "react";
+import { Select, Switch } from "antd";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import CreateAccount from "./components/CreateAccount.js";
 import RecoverAccount from "./components/RecoverAccount.js";
 import WalletView from "./components/WalletView.js";
+import logoLight from "./biologothree.png";
+import logoDark from "./biodarklogothree.png";
 import "@innovatrics/dot-auto-capture-ui/face";
+import "./App.css";
 
 const FaceCamera = (props) => {
   useEffect(() => {
@@ -22,9 +21,7 @@ const FaceCamera = (props) => {
     }
   });
 
-  return React.createElement("x-dot-face-auto-capture", {
-    id: "x-dot-face-auto-capture",
-  });
+  return <x-dot-face-auto-capture id="x-dot-face-auto-capture" />;
 };
 
 const FaceUi = (props) => {
@@ -36,14 +33,14 @@ const FaceUi = (props) => {
     }
   });
 
-  return React.createElement("x-dot-face-auto-capture-ui", {
-    id: "x-dot-face-auto-capture-ui",
-  });
+  return <x-dot-face-auto-capture-ui id="x-dot-face-auto-capture-ui" />;
 };
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   const [wallet, setWallet] = useState(null);
   const [seedPhrase, setSeedPhrase] = useState(null);
+  const [showCreateWallet, setShowCreateWallet] = useState(false);
   const [selectedChain, setSelectedChain] = useState("0x1");
 
   const handleFacePhotoTaken = (image, resolution) => {
@@ -56,84 +53,76 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? "dark" : "light"}`}>
       <header>
-        <img src={logo} className="headerLogo" alt="logo" />
-        <div className="themeToggler"></div>
-        <Select
-          onChange={(val) => setSelectedChain(val)}
-          value={selectedChain}
-          options={[
-            { label: "Ethereum", value: "0x1" },
-            { label: "Mumbai", value: "0x13881" },
-            { label: "Avalanche", value: "0xa86a" },
-            { label: "Binance", value: "0x38" },
-          ]}
-          className="dropdown"
-        ></Select>
+        <div className="headerContent">
+          <img
+            src={darkMode ? logoDark : logoLight}
+            className="headerLogo"
+            alt="logo"
+          />
+          <Switch
+            checked={darkMode}
+            onChange={() => setDarkMode((prevMode) => !prevMode)}
+            checkedChildren="Dark Mode"
+            unCheckedChildren="Light Mode"
+            className="themeToggler"
+          />
+          <Select
+            onChange={(val) => setSelectedChain(val)}
+            value={selectedChain}
+            options={[
+              { label: "Ethereum", value: "0x1" },
+              { label: "Avalanche", value: "0xa86a" },
+              { label: "Binance", value: "0x38" },
+            ]}
+            className="dropdown"
+          />
+        </div>
       </header>
-      {wallet && seedPhrase ? (
-        <Routes>
-          <Route
-            path="/yourwallet"
-            element={
-              <WalletView
-                wallet={wallet}
-                setWallet={setWallet}
-                seedPhrase={seedPhrase}
-                setSeedPhrase={setSeedPhrase}
-                selectedChain={selectedChain}
-              />
-            }
-          />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/recover"
-            element={
-              <RecoverAccount
-                setSeedPhrase={setSeedPhrase}
-                setWallet={setWallet}
-              />
-            }
-          />
-          <Route
-            path="/yourwallet"
-            element={
-              <>
-                <div style={{ position: "relative" }}>
+      <div className="content">
+
+        {wallet && seedPhrase ? (
+          <Routes>
+            <Route
+              path="/yourwallet"
+              element={
+                <WalletView
+                  wallet={wallet}
+                  setWallet={setWallet}
+                  seedPhrase={seedPhrase}
+                  setSeedPhrase={setSeedPhrase}
+                  selectedChain={selectedChain}
+                />
+              }
+            />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/recover"
+              element={
+                <RecoverAccount
+                  setSeedPhrase={setSeedPhrase}
+                  setWallet={setWallet}
+                />
+              }
+            />
+            <Route
+              path="/yourwallet"
+              element={
                 <CreateAccount
                   setSeedPhrase={setSeedPhrase}
                   setWallet={setWallet}
                 />
-                <FaceCamera
-                  imageType="png"
-                  cameraFacing="user"
-                  onPhotoTaken={handleFacePhotoTaken}
-                  onError={handleError}
-                />
-                <FaceUi
-  
-                />
-                </div>
-              </>
-            }
-          />
-          <Route
-            path="/biometrics"
-            element={
-              <RecoverAccount
-                setSeedPhrase={setSeedPhrase}
-                setWallet={setWallet}
-              />
-            }
-          />
-        </Routes>
-      )}
+              }
+            />
+          </Routes>
+        )}
+      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
